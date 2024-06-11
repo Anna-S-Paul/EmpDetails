5d97136a682c4dd06c18c8510ed74c0b1c95d916
AnnaJ <annaspaulimp@gmail.com>
2024-06-11 16:08:12 +0530

    updated

--- src/components/AddEmployee.jsx
+++ src/components/AddEmployee.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavEmp from './NavEmp'
+import axios from 'axios'
 
 const AddEmployee = () => {
+    const [data,changeData]=useState(
+        {
+            "empid":"",
+            "empname":"",
+            "designation":"",
+            "salary":""
+        }
+    )
+    const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+        axios.post("http://localhost:8081/",data).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status=="success") {
+                    alert("successfully added")
+                } else {
+                    alert("error")
+                }
+            }
+        ).catch().finally()
+    }
     return (
         <div>
             <NavEmp/>
@@ -12,35 +37,35 @@ const AddEmployee = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Emp ID:</label>
-                                <input type="number" name="" id="" className="form-control" />
+                                <input type="number" name="empid" id="" className="form-control" value={data.empid} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Emp name:</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='empname' onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Designation:</label>
-                                <select name="" id="" className="form-control">
+                                <select name="designation" id="" className="form-control" value={data.designation} onChange={inputHandler}>
                                     <option value="s">--select--</option>
-                                    <option value="ma">Manager</option>
+                                    <option value="manager">Manager</option>
                                     <option value="hr">HR</option>
-                                    <option value="int">Intern</option>
+                                    <option value="intern">Intern</option>
                                 </select>
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Salary:</label>
-                                <input type="number" name="" id="" className="form-control" />
+                                <input type="number" name="salary" id="" className="form-control" value={data.salary} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                                <button className="btn btn-success">Add</button>
+                                <button className="btn btn-success" onClick={readValue}>Add</button>
 
                             </div>
                         </div>
--- src/components/ViewEmp.jsx
+++ src/components/ViewEmp.jsx
@@ -1,52 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavEmp from './NavEmp'
+import axios from 'axios'
 
 const ViewEmp = () => {
-  const [data,changeData]=useState(
-    [
-      {"id":101, "Name":"Ann","Designation":"Manager", "salary":1500},
-      {"id":102, "Name":"Anna","Designation":"HR", "salary":45670},
-      {"id":908, "Name":"Alfiya","Designation":"Manager", "salary":23000},
-      {"id":232, "Name":"Aseera","Designation":"Intern", "salary":10000},
-      {"id":243, "Name":"Aysha","Designation":"Intern", "salary":10000}
-    ]
-  )
+  const [data, changeData] = useState([])
+  const fetchData = () => {
+    axios.get("http://localhost:8081/viewall").then(
+      (response) => {
+        changeData(response.data)
+      }
+    ).catch().finally()
+  }
+  useEffect(() => {fetchData()}, [])
   return (
     <div>
-        <NavEmp/>
-        <div className="container">
-            <div className="row g-3">
-                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+      <NavEmp />
+      <div className="container">
+        <div className="row g-3">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Emp ID</th>
-      <th scope="col">Emp Name</th>
-      <th scope="col">Designation</th>
-      <th scope="col">salary</th>
-    </tr>
-  </thead>
-  <tbody>
-   {
-    data.map(
-      (value,index)=>{
-        return  <tr>
-        <td>{value.id}</td>
-        <td>{value.Name}</td>
-        <td>{value.Designation}</td>
-        <td>{value.salary}</td>
-      </tr>
-      }
-    )
-   }
-    
-  </tbody>
-</table>
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Emp ID</th>
+                  <th scope="col">Emp Name</th>
+                  <th scope="col">Designation</th>
+                  <th scope="col">salary</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  data.map(
+                    (value, index) => {
+                      return <tr>
+                        <td>{value.empid}</td>
+                        <td>{value.empname}</td>
+                        <td>{value.designation}</td>
+                        <td>{value.salary}</td>
+                      </tr>
+                    }
+                  )
+                }
 
-                </div>
-            </div>
+              </tbody>
+            </table>
+
+          </div>
         </div>
+      </div>
     </div>
   )
 }
